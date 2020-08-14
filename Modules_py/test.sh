6cba2a727b47d58a3d2d59add10c322e853e91ae
Amrit <amritsingh.d12@gmail.com>
2020-08-14 13:39:24 +0530

    Modules Project Created - 08142020

---
+++ Modules_py/test.sh
@@ -0,0 +1,7 @@
+x=12
+if [[ $x -lt 12 ]]
+then
+  echo "x is smaller than 12"
+else
+  echo "x is larger than or equal than 12"
+fi
---
+++ test_scripts/test1.py
@@ -0,0 +1,5 @@
+import os
+
+print(os.getcwd())
+a_str = "Hello World"
+print(a_str)
