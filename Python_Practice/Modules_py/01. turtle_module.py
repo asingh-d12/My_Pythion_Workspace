f0a65a77e227ec7172936cd305517037e7b95577
Amrit <amritsingh.d12@gmail.com>
2020-08-14 14:41:59 +0530

    Module Started - 08142020

---
+++ Python_Practice/Modules_py/01. turtle_module.py
@@ -0,0 +1,13 @@
+import turtle
+# Turtle module is used to emulate LOGO
+
+# Let's now run a few commands in turtle module
+turtle.color("blue")
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
+turtle.circle(-100)
+
+# Right now a box comes on screen, executes the command and then close down
+# WE do have a way to make sure that it stops, done method
+turtle.done()
