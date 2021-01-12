b8288528b39a648143a5f56cbdfaa3b7b53cb15e
amrit <amritsingh.d12@gmail.com>
2021-01-12 21:42:18 +0530

    12 Jan - Python_Revision_2021

---
+++ Python_Revision_2021/if_elif_else/guessing_game.py
@@ -0,0 +1,32 @@
+import random
+
+max_num = 1000
+
+print("Let's start with this game!!\nGuess between 1 and {}".format(max_num))
+
+print("*" * 50)
+
+while True:
+    num_to_guess = random.randint(1, max_num)
+    print("Number to guess is chosen, Let's continue!!!")
+
+    num_of_chances = 0
+
+    while True:
+        num_of_chances += 1
+        num = int(input("Guess the number : "))
+
+        if num_to_guess > num:
+            print("Guess Higher!!")
+        elif num_to_guess < num:
+            print("Guess Lower!!")
+        else:
+            print("Congrats!! You guessed it on your try number = {}".format(num_of_chances))
+            break
+
+    print("*" * 100)
+    quit_game = input("Press y to quit, Just enter to continue again : ")
+    if quit_game == 'y':
+        break
+
+    print("*" * 100)
